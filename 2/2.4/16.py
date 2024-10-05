33fe61f044629feb2cfb2eb902cfdaaa656a75a4
MIkhail Khorokhorin <mikhailkhorokhorin@gmail.com>
2024-10-05 05:59:03 +0300

    Add solutions for 16 and 20 tasks

--- 2/2.4/16.py
+++ 2/2.4/16.py
@@ -1,18 +1,22 @@
 def main():
     n, m = [int(input()) for _ in range(2)]
-    string = n * m + n - 1
-    if n == 1:
-        print(" " + str(n))
-    else:
+    string = m * n + (n - 1)
+    if n != 1:
         for i in range(1, n + 1):
             for j in range(1, n + 1):
-                print(f"{i * j:^{m}}", end="")
-                if j == n:
-                    print()
+                if j != n:
+                    k = str(i * j)
+                    result = f'{k: ^{m}}'
+                    print(result, '|', sep="", end="")
                 else:
-                    print("|", end="")
-            if i != m:
-                print("-" * string)
+                    k = str(i * j)
+                    result = f'{k: ^{m}}'
+                    print(result, end="")
+            print()
+            if i != n:
+                print('-' * string)
+    else:
+        print(f"{1: ^{m}}")
 
 
 if __name__ == "__main__":
--- 2/2.4/20.py
+++ 2/2.4/20.py
@@ -1,16 +1,27 @@
 def main():
     n = int(input())
-    max_i, max_num = 0, 0
-    for i in range(2, 11):
-        num = n
-        s = []
-        while num > 0:
-            s = [num % i] + s
-            num //= i
-        if max_num < sum(s):
-            max_num = sum(s)
-            max_i = i
-    print(max_i)
+    max_length, number, line = 0, 0, 1
+    while number < n:
+        length = ""
+        for i in range(line):
+            if number < n:
+                number += 1
+                length += str(number) + " "
+            else:
+                break
+        line += 1
+        max_length = max(len(length[:-1]), max_length)
+    number, line = 0, 1
+    while number < n:
+        answer = ""
+        for i in range(line):
+            if number < n:
+                number += 1
+                answer += str(number) + " "
+            else:
+                break
+        line += 1
+        print(f"{answer[:-1]:^{max_length}}")
 
 
 if __name__ == "__main__":
